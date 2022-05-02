1f8da3f8eaefbde0c21b7280ee8000a5d972161d
Igor V <csfolder@gmail.com>
2022-05-02 20:47:14 +0300

    Some changes made!

--- PassGen/settings.py
+++ PassGen/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3)7_^5^v=hxcr_1d_=409-vvbi3pv4$#&px9-(bk(kxbuex^z9'
+SECRET_KEY = 'django-insecure-3)7_^5^v=hxcr_1asdadfsdgsdfasdasfdd_=409-vvbi3pv4$#&px9-(bk(kxbuex^z9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
